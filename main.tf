6af5e370f7f6323b6fdfaf3a13b121c698edb515
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 10:56:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_vpc" "main" {
 }
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  
   cidr_block = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = true
