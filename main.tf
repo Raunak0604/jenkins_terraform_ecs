704dfeeb89e2ed00320f839c36e1dbbc56955971
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 11:46:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,59 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+ 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "TF-SG"
   }
 }
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+ 
+  tags = {
+    Name = "TF-SG"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+ 
+  tags = {
+    Name = "TF-GW"
+  }
+}
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "TF-RT"
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.main2.id
+  route_table_id = aws_route_table.rt.id
+}
+
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
   #   ingress {
   #   from_port        = 0
   #   to_port          = 0
@@ -41,4 +75,121 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_ecs_cluster" "foo" {
+  name = "white-hart"
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "execution-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+# "image": "879580385544.dkr.ecr.ap-south-1.amazonaws.com/gitlab-ecr:latest"
+resource "aws_ecs_task_definition" "aws-ecs-task" {
+  family = "task"
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "container",
+      "image": "${var.imageUri}",
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "cpu": 256,
+      "memory": 512,
+      "networkMode": "awsvpc"
+    }
+  ]
+  DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "512"
+  cpu                      = "256"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+}
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "ecs-service"
+  cluster              = aws_ecs_cluster.foo.id
+  task_definition      = aws_ecs_task_definition.aws-ecs-task.id
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
+
+  network_configuration {
+    subnets          = [aws_subnet.main.id]
+    assign_public_ip = true
+    security_groups = [
+      aws_security_group.allow_tls.id,
+    ]
+  }
+    load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "container"
+    container_port   = 5000
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
+
+  resource "aws_lb" "test" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_tls.id]
+  subnets            = [aws_subnet.main.id, aws_subnet.main2.id]
+
+   ip_address_type    = "ipv4"
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "300"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/v1/status"
+    unhealthy_threshold = "2"
+  }
+}
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.test.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
+  }
+}
