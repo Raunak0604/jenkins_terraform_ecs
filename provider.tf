1130ce402d1d86f524aa56e796bb4873062c5883
Raunak Mondal <101201579+Raunak0604@users.noreply.github.com>
2023-03-10 17:56:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
 # Configure and downloading plugins for aws
 provider "aws" {
