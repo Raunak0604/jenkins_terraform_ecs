1c9a8b1cd3c77e715089b638da9e04dcaf37a7cb
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 10:54:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,31 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+ 
+  tags = {
+    Name = "TF-SG"
+  }
+}
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+ 
+  tags = {
+    Name = "TF-SG"
+  }
+}
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
   #   ingress {
   #   from_port        = 0
   #   to_port          = 0
