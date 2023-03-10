e5680e82baed12b7630cf624401d88b4219bf0d7
Mustak Ahmed <72276954+Mustak-Ahmed@users.noreply.github.com>
2023-03-10 12:18:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 4.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 terraform {
   required_version = ">= 0.12"
 }
